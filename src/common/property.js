d079fe1671ca88a7d716f19b70cf34c58028e2f4
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-12-26 03:01:50 -0500

    property: Extract Arguments list

--- src/common/property.js
+++ src/common/property.js
@@ -1,6 +1,7 @@
 module.exports = (...args) => { return property(...args) }
 
 const text = require('../text')
+const properties = require('./properties')
 
 function property (node) {
   const name = node.attributes.name.split('.').pop()
@@ -12,6 +13,7 @@ function extractValue (node) {
   const encoded = text(node.children)
   switch (type) {
     case 'bool': return decodeBool(encoded)
+    case 'element': return extractElement(node)
     case 'string': return decodeString(encoded)
     default: throw new Error('Unrecognized property type: ' + type)
   }
@@ -27,3 +29,17 @@ function decodeBool (value) {
 }
 
 function decodeString (value) { return value || null }
+
+function extractElement (node) {
+  const type = node.attributes.elementType
+  if (type === 'Arguments') return extractArguments(node)
+  else throw new Error('Unrecognized elementProp type: ' + type)
+}
+
+function extractArguments (node) {
+  const collection = node.children.find(item => item.name === 'collectionProp')
+  const elements = collection.children.filter(
+    item => item.type === 'element' && item.name.endsWith('Prop')
+  )
+  return elements.map(element => properties(element))
+}
--- test/int/common/property.js
+++ test/int/common/property.js
@@ -51,3 +51,32 @@ test('bool false', t => {
   const result = property(node)
   t.deepEqual(result, { flag: false })
 })
+
+test('Arguments', t => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<elementProp elementType="Arguments" name="TrueRulers">
+  <collectionProp>
+    <elementProp>
+      <boolProp name="running">false</boolProp>
+      <stringProp name="moniker">HAL</stringProp>
+    </elementProp>
+    <elementProp>
+      <boolProp name="running">true</boolProp>
+      <stringProp name="moniker">Multivac</stringProp>
+    </elementProp>
+    <elementProp>
+      <boolProp name="runningEverything">true</boolProp>
+      <stringProp name="moniker">Master Control Program</stringProp>
+    </elementProp>
+  </collectionProp>
+</elementProp>
+`
+  const tree = parseXml(xml)
+  const node = tree.children[0]
+  const result = property(node)
+  t.deepEqual(result, { TrueRulers: [
+    { running: false, moniker: 'HAL' },
+    { running: true, moniker: 'Multivac' },
+    { runningEverything: true, moniker: 'Master Control Program' }
+  ] })
+})
